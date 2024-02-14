3999b00fd088737d18ace9725efd0b97b92514e7
matheus <cardosomcds1@gmail.com>
2024-02-14 12:39:44 -0300

    Segmento nos estudos POO

---
+++ python_udemy/POO/encapsulamento.py
@@ -0,0 +1,11 @@
+﻿"""
+public = pode ser acessada de qualquer lugar
+proteced = Só é acessado na sua classe e suas sub classes
+private = Só pode ser usado em sua classe 
+
+"""
+class Foo:
+    def __init__(self) -> None:
+        self.public = 'publico'
+        self._protected = 'acesso na sua classe e suas sub classes'
+        self.__private  = 'Acesso apenas em sua classe'
--- python_udemy/POO/get_set.py
+++ python_udemy/POO/get_set.py
@@ -1,12 +1,33 @@
 ﻿class Caneta:
     def __init__(self, cor):
-        self.cor_tinta = cor
+        self._cor = cor
+        self._cor_tampa = None
         
         
     @property
     def cor(self):
-        return self.cor_tinta
+        print('GETTER')
+        return self._cor
     
+    @cor.setter
+    def cor(self, valor):
+        print('SETTER')
+        self._cor = valor
+    
+    @property
+    def cor_tampa(self):
+        print('GETTER')
+        return self._cor_tampa
+    
+    @cor_tampa.setter
+    def cor_tampa(self, nova_cor_tampa):
+        print('SETTER')
+        self._cor_tampa = nova_cor_tampa
+
+
     
 caneta = Caneta('Azul')
-print (caneta.cor)
+caneta.cor_tampa = 'Roxo'
+caneta.cor = 'rosa'
+print(caneta.cor_tampa)
+#print (caneta.cor)
