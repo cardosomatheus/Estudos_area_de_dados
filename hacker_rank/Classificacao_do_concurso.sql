ce55a8258ef68a3d4e29640b377d5c82b5436fd3
matheus <cardosomcds1@gmail.com>
2024-08-24 12:15:18 -0300

    Desafios de sql sobre CTE e joins.

---
+++ hacker_rank/Classificacao_do_concurso.sql
@@ -0,0 +1,37 @@
+﻿ Você fez um trabalho tão bom ajudando Julia com seu último desafio de competição de codificação que ela quer que você trabalhe neste também!
+ A pontuação total de um hacker é a soma de suas pontuações máximas para todos os desafios.
+Escreva uma consulta para imprimir o hacker_id , o nome e a pontuação total dos hackers ordenados pela pontuação decrescente. Se mais de um hacker atingiu a mesma pontuação total,
+então classifique o resultado por hacker_id crescente . 
+Exclua todos os hackers com uma pontuação total de 0 no seu resultado.
+
+
+Hackers: O hacker_id é o id do hacker, e o name é o nome do hacker.
+SUBMISSIONS: submission_id é o id do envio, hacker_id é o id do hacker que fez o envio, challenge_id é o id do desafio ao qual o envio pertence e score é a pontuação do envio.
+
+
+
+WITH A AS (
+  -- PONTUAÇÃO MAXIMA EM CADA DESAFIO FEITO PELO HACKER 
+  SELECT H.HACKER_ID,
+         H.NAME,
+         S.CHALLENGE_ID,
+         MAX(S.SCORE) MAXPONT_POR_DESAFIO_USER
+    FROM SUBMISSIONS S
+    JOIN HACKERS H ON S.HACKER_ID = H.HACKER_ID
+   GROUP BY H.HACKER_ID, H.NAME, S.CHALLENGE_ID 
+),
+B AS ( 
+  -- SOMANDO AS PONTUAÇÕES MAXIMA DOS DESAFIOS POR HACKER
+  SELECT A.HACKER_ID,
+         A.NAME,
+         SUM(A.MAXPONT_POR_DESAFIO_USER) SUMPONT_POR_DESAFIO_USER
+    FROM A
+   GROUP BY A.HACKER_ID, A.NAME
+)
+-- RETORNANDO APENAS AQUELE POSSUI UMA PONTUAÇÃO MAIOR QUE 0
+SELECT B.HACKER_ID,
+       B.NAME,
+       B.SUMPONT_POR_DESAFIO_USER
+  FROM B 
+  WHERE B.SUMPONT_POR_DESAFIO_USER > 0
+  ORDER BY B.SUMPONT_POR_DESAFIO_USER DESC, B.HACKER_ID;
---
+++ hacker_rank/desafios_por_hackers.sql
@@ -0,0 +1,33 @@
+﻿ Julia pediu que seus alunos criassem alguns desafios de codificação. Escreva uma consulta para imprimir o hacker_id , o nome e o número total de desafios criados por cada aluno.
+lassifique seus resultados pelo número total de desafios em ordem decrescente. Se mais de um aluno criou o mesmo número de desafios, classifique o resultado por hacker_id . 
+Se mais de um aluno criou o mesmo número de desafios e a contagem for menor que o número máximo de desafios criados, exclua esses alunos do resultado.
+
+
+Hackers: O hacker_id é o id do hacker, e o name é o nome do hacker.
+
+Challenges: O challenge_id é o id do desafio, e o hacker_id é o id do aluno que criou o desafio.
+
+
+WITH A AS (
+    -- A quantidade de desafios por hackers
+    SELECT H.hacker_id, h.name, COUNT(D.challenge_id)  contador_desafios
+      FROM Challenges D
+      JOIN HACKERS H ON D.hacker_id = H.hacker_id
+     GROUP BY H.hacker_id, H.name
+),
+B AS (
+    -- valor de desafios
+    SELECT MAX(A.contador_desafios) max_contador_desafios FROM A
+),
+C AS (
+    -- informa os desafios que possui uma contagem de desafios menores do que o valor maximo e se repetem mais de uma vez para diferentes hackes.
+    SELECT A.contador_desafios, COUNT(*) 
+      FROM A,B
+     WHERE A.contador_desafios < B.max_contador_desafios
+     GROUP BY A.contador_desafios
+     HAVING COUNT(*) > 1
+)
+SELECT * 
+    FROM A
+ WHERE A.contador_desafios NOT IN (SELECT C.contador_desafios FROM C)
+ ORDER BY A.contador_desafios DESC, A.HACKER_ID;
