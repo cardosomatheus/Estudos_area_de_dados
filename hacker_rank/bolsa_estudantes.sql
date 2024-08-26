4f45a7c50289ddf5f4a0dd93a03a6f61b28f5b6c
matheus <cardosomcds1@gmail.com>
2024-08-26 09:01:06 -0300

    exercicios em joins.

---
+++ hacker_rank/bolsa_estudantes.sql
@@ -0,0 +1,17 @@
+﻿ Você recebe três tabelas:  Students , Friends e Packages.  
+
+Students contém duas colunas: ID  e Name .
+Friends contém duas colunas: ID e Friend_ID ( ID do ÚNICO melhor amigo).
+Packages  contém duas colunas: ID e Salary (salário oferecido em $ milhares por mês).
+
+
+ Escreva uma consulta para gerar os nomes dos alunos cujos melhores amigos receberam uma oferta de salário maior que a deles. Os nomes devem ser ordenados
+pelo valor do salário oferecido aos melhores amigos. É garantido que nenhum aluno recebeu a mesma oferta de salário.
+
+SELECT D.NAME 
+  FROM Friends A
+  JOIN Packages B ON A.ID = B.ID
+  JOIN Packages C ON A.FRIEND_ID = C.ID
+  JOIN Students D ON A.ID = D.ID
+ WHERE C.SALARY > B.SALARY
+ ORDER BY C.SALARY;
---
+++ hacker_rank/cidades_dos_eua.sql
@@ -0,0 +1,7 @@
+﻿Consulte todas as colunas para todas as cidades americanas na tabela CITY com populações maiores que 100000. O CountryCode para América é USA.
+
+
+SELECT * 
+  FROM CITY
+ WHERE UPPER(COUNTRYCODE) = 'USA'
+ AND POPULATION > 100000;
